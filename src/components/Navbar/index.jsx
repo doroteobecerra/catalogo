e5ec0ce6f2924a177ef307b469cb90dc36afb8a2
Alejandro Doroteo <alejandro.doroteo@coppel.com>
2025-01-29 12:52:15 -0600

    Se agrega estilos a navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,61 +1,91 @@
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+
+    const activeStyle = 'underline'
+
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm">
+            <ul className="flex items-center gap-3">
+                <li className='font-semibold text-lg'>
                     <NavLink to='/'>
                         Shopi
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink 
+                        to='/'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         All
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/clothes'>
+                    <NavLink 
+                        to='/clothes'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         Clothes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/electronics'>
+                    <NavLink 
+                        to='/electronics'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         Electronics
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/furnitues'>
+                    <NavLink 
+                        to='/furnitues'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         Furnitues
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/toys'>
+                    <NavLink 
+                        to='/toys'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         Toys
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/others'>
+                    <NavLink 
+                        to='/others'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         Others
                     </NavLink>
                 </li>
             </ul>
-            <ul>
-                <li>
+            <ul className="flex items-center gap-3">
+                <li className="text-black/60">
                     doroteo
                 </li>
                 <li>
-                    <NavLink to='/my-orders'>
+                    <NavLink 
+                        to='/my-orders'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         My Orders
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/my-account'>
+                    <NavLink 
+                        to='/my-account'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         My Account
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/sing-in'>
+                    <NavLink 
+                        to='/sing-in'
+                        className={( {isActive} ) => 
+                            isActive ? activeStyle : undefined}>
                         Sing In
                     </NavLink>
                 </li>
