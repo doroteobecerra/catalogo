9e8c41a3d3c7efa27ea7095884b2b4b17e0a7cf1
Alejandro Doroteo <doroteo@Mac-mini-de-Alejandro.local>
2025-02-23 19:36:18 -0600

    Se consume API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
 import Layout from "../../components/Layout"
 import Card from "../../components/Card"
+import { useEffect, useState } from "react"
 
 function Home() {
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(res => res.json())
+      .then(data => setItems(data))
+      
+      console.log(items)
+
+  }, [])
 
   return (
       <Layout>
           Home
-          <Card></Card>
+          <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+            {items?.map(item=>(
+              <Card key={item.id} data={item}/>
+            ))} 
+          </div>
       </Layout>
   )
 }
